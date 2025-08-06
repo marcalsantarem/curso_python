4092e5d46526f40a044a8683039b6ea8b57aced0
Marcal <marcalsantarem@gmail.com>
2025-08-06 16:05:15 -0300

    Aula básica sobre comentários no python

---
+++ aulas/aula1.py
@@ -0,0 +1,9 @@
+# Permite escrever um comentário
+print(123) # Na frente
+# Abaixo
+print(456)
+"""
+    Isso se chama DOCSTRING
+    Pode ser usado como comentário
+    Mas é lido pelo interpretador, diferente dos comentários com #
+"""
