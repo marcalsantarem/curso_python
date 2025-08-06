eb90f1aabcbad59d4fcfc561b4a702b746deccd9
Marcal <marcalsantarem@gmail.com>
2025-08-06 17:04:31 -0300

    Aula explicativa sobre o tipo string

---
+++ aulas/aula3.py
@@ -0,0 +1,21 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Marçal Santarém')
+print('1, Marçal "Santarém"')
+
+# Aspas duplas
+print("Marçal Santarém")
+print(2, "Marçal 'Santarém'")
+
+# Escape
+print("Marçal \"Santarém\"")
+
+# r
+print(r"Marçal \"Santarém\"")
