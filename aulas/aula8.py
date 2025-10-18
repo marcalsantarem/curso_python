ed94ed12ae08a52f8f8a282ac156756650bd2750
Marcal <marcalsantarem@gmail.com>
2025-10-18 14:42:27 -0300

    Aula 8 - Exercício sobre tipos de variáveis

---
+++ aulas/aula8.py
@@ -0,0 +1,28 @@
+# EXERCÍCIO
+#
+# Criar variáveis para todos os dados das impressões abaixo
+#
+# print('Nome:', nome)
+# print('Sobrenome:', sobrenome)
+# print('Idade:', idade)
+# print('Ano de nascimento:', ano_nascimento)
+# print('É maior de idade?', maior_de_idade)
+# print('Altura em metros:', altura_metros)
+
+nome = 'Marçal'
+print('Nome:', nome)
+
+sobrenome = 'Santarém'
+print('Sobrenome:', sobrenome)
+
+idade = 32
+print('Idade:', idade)
+
+ano_nascimento = 2025 - idade
+print('Ano de nascimento:', ano_nascimento)
+
+maior_de_idade = idade >= 18
+print('É maior de idade?', maior_de_idade)
+
+altura_metros = 1.75
+print('Altura em metros:', altura_metros)
